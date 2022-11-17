4f837b459419ac70e5c03fafecec2ecdb6377230
Chaos Sukhinsky <kos368437@yandex.ru>
2022-11-18 00:30:45 +0700

    commit-all

---
+++ src/test/kotlin/ru/nsu/ccfit/sukhinsky/ApplicationTest.kt
@@ -0,0 +1,40 @@
+package ru.nsu.ccfit.sukhinsky
+
+import io.ktor.server.routing.*
+import io.ktor.http.*
+import io.ktor.server.auth.*
+import io.ktor.util.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.websocket.*
+import io.ktor.websocket.*
+import java.time.Duration
+import io.ktor.network.selector.*
+import io.ktor.network.sockets.*
+import io.ktor.utils.io.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import java.io.InputStream
+import java.util.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.request.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import kotlin.test.*
+import io.ktor.server.testing.*
+import ru.nsu.ccfit.sukhinsky.plugins.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            configureRouting()
+        }
+        client.get("/").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello World!", bodyAsText())
+        }
+    }
+}
